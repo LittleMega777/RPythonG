f66ffc9f1fce0766f8b487d9a4feb8e59f06ca59
T783860 <jean.junior@f1rst.com.br>
2024-10-11 14:57:39 -0300

    feat(character.py, mob.py) - ajuste de indentacao

--- classes/character.py
+++ classes/character.py
@@ -1,28 +1,28 @@
 class Character:
   
-  def __init__(self):
-    self.name = "Guerreiro"
-    self.max_life = 10
-    self.life = self.max_life
-    self.damage = 3
-    self.max_xp = 10
-    self.xp = 0
-    self.level = 1
-    self.max_stamina = 3
-    self.stamina = 0
-    self.stamina_regen = 1
-    self.coins = 0
-  
-  @property
-  def alive(self):
-    return self.life >= 0
-  
-  def show_stats(self):
-    print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage} // Level: {self.level} // XP: {self.xp}/{self.max_xp} // Moedas: {self.coins}")
-  
-  def show_battle_stats(self):
-    print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage}")
+	def __init__(self):
+		self.name = "Guerreiro"
+		self.max_life = 10
+		self.life = self.max_life
+		self.damage = 3
+		self.max_xp = 10
+		self.xp = 0
+		self.level = 1
+		self.max_stamina = 3
+		self.stamina = 0
+		self.stamina_regen = 1
+		self.coins = 0
+	
+	@property
+	def alive(self):
+		return self.life >= 0
+	
+	def show_stats(self):
+		print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage} // Level: {self.level} // XP: {self.xp}/{self.max_xp} // Moedas: {self.coins}")
+	
+	def show_battle_stats(self):
+		print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage}")
 
-  def character_attack(self, target):
-    target.life -= self.damage
+	def character_attack(self, target):
+		target.life -= self.damage
     
--- classes/mob.py
+++ classes/mob.py
@@ -1,25 +1,26 @@
+import random
+
 class Monster():
   
-  def __init__(self, name):
-    self.name = name
-    self.max_life = 10
-    self.life = self.max_life
-    self.damage = 1
-    self.max_stamina = 6
-    self.stamina = 0
-    self.stamina_regen = 1
+	def __init__(self, name):
+		self.name = name
+		self.max_life = 10
+		self.life = self.max_life
+		self.damage = 1
+		self.max_stamina = 6
+		self.stamina = 0
+		self.stamina_regen = 1
 
-  @property
-  def alive(self):
-    return self.life >= 0
-  
-  def show_monster_stats(self):
-    print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage}")
+	@property
+	def alive(self):
+		return self.life >= 0
 
-  def show_battle_stats(self):
-    print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage}")  
+	def show_monster_stats(self):
+		print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage}")
 
-  ## criar status de stamina por um sleep para contar como stamina
-  def monster_attack(self, target):
-    target.life -= self.damage
-    
+	def show_battle_stats(self):
+		print(f"Name: {self.name} // HP: {self.life}/{self.max_life} // Damage: {self.damage}")  
+
+	def monster_attack(self, target):
+		target.life -= self.damage
+
